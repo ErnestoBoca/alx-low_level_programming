531dcab93e9786720f83716ae2935e753cff868b
Ernesto Boca <boca.ernesto13@gmail.com>
2023-08-10 08:58:25 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main - Prints a message 
+ * main - Entry point
  *
- * REturn: Always 0
+ * Return: Always 0 c
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
