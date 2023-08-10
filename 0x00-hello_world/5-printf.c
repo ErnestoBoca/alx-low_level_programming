0966c2d40ad8340b3fa6e7eb31f60283d8361637
Ernesto Boca <boca.ernesto13@gmail.com>
2023-08-10 09:05:35 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Prints a message
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n";
+	return (0);
+}
