784d8e017bb40a41ba85a4ffc3b116211e920edd
Ernesto Boca <boca.ernesto13@gmail.com>
2023-08-11 06:17:37 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints number from 0 to 10
+ *
+ * Return: always 0 (default)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}
