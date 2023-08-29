a8137e5a9f513b5dbece7c799fe2ba699ae28dc4
Ernesto Boca <boca.ernesto13@gmail.com>
2023-08-28 17:11:34 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+/**
+ * _strchr - locates a character in a string.
+ * @s: The string
+ * @c: the character to be located
+ * Return:  pointer to the first occurrence of the character c
+ */
+char *_strchr(char *s, char c)
+{
+	char *p, chr;
+	int i;
+
+	i = 0;
+	chr = s[i];
+	p = 0;
+	while (chr != '\0')
+	{
+		if (chr == c)
+		{
+			p = &s[i];
+			break;
+		}
+		i++;
+		chr = s[i];
+	}
+	return (p);
+}
