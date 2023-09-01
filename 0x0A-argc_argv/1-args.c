5cbf4b8d0e01d38700b96616c1c92e7ccb1be85c
Ernesto Boca <boca.ernesto13@gmail.com>
2023-09-01 08:39:59 -0700

    ./nargs hello, world

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints its name, followed by a new line.
+ * @args: the number of arguments
+ * @argv: the vector of arguments
+ * Return: always 0
+ */
+int main(int args, char *argv[])
+{
+        (void)argv;
+	printf("%d\n", args - 1);
+	return (0);
+}
