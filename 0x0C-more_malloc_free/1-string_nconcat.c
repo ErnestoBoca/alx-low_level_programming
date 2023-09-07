72d2885d1f4a1425544f93eb9e5e66bbeb35b537
Ernesto Boca <boca.ernesto13@gmail.com>
2023-09-07 15:02:07 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,17 +17,15 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	l1 = 0;
 	l2 = 0;
-	if (s1 != NULL)
-	{
-		for (i = 0; s1[i] != '\0'; i++)
-			l1++;
-	}
+	if (s1 == NULL)
+		s1 = "";
+	for (i = 0; s1[i] != '\0'; i++)
+		l1++;
 	if (s2 != NULL)
-	{
-		for (i = 0; s2[i] != '\0'; i++)
-			l2++;
-	}
-	ptr = malloc(sizeof(char) * (l1 + l2 + 1 + 1));
+		s2 = "";
+	for (i = 0; s2[i] != '\0'; i++)
+		l2++;
+	ptr = malloc(sizeof(char) * (l1 + n + 1));
 	if (ptr == NULL)
 		return (NULL);
 	j = 0;
