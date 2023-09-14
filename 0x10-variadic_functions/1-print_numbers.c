8a545e0b0cb6143b5340a72e08ff40aec31e34fa
Ernesto Boca <boca.ernesto13@gmail.com>
2023-09-14 07:00:46 -0700

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,12 +11,17 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list args;
 
+	if (separator == NULL)
+		return;
+
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(args, int));
-		if (separator != NULL && i != n - 1)
+		if (i != n - 1)
+		{
+			printf("%d", va_arg(args, int));
 			printf(", ");
+		}
 	}
 	printf("\n");
 }
