cf4748ec4a72e423bc6504944be5bcff08d2c18f
Ernesto Boca <boca.ernesto13@gmail.com>
2023-09-14 09:03:48 -0700

    mini-printf

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -40,10 +40,6 @@ void print_all(const char * const format, ...)
 				printf("%s", str);
 				break;
 		}
-		if (format[i + 1] != '\0' && (format[i] == 'c' || format[i] == 'i'
-					|| format[i] == 'f' || format[i] == 's'))
-			printf(", ");
-
 		i++;
 		c = format[i];
 	}
