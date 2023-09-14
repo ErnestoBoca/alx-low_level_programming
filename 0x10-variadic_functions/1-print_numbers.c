d388a234de769c0a3b3ca445c8e9bd47302c7281
Ernesto Boca <boca.ernesto13@gmail.com>
2023-09-14 07:19:38 -0700

    Print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -20,9 +20,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		printf("%d", va_arg(args, int));
 		if (separator != NULL && i != n - 1)
 		{
-			printf(", ");
+			printf("%s ", separator);
 		}
 	}
-	va_end(args);
 	printf("\n");
+	va_end(args);
 }
