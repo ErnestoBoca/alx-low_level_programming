24fb3b00d7241be578e1b27b72dac37d9453786b
Ernesto Boca <boca.ernesto13@gmail.com>
2023-09-22 16:10:44 -0700

    Correcting betty

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,19 +10,19 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new_node;
 
 	if (str == NULL)
-		return NULL;
+		return (NULL);
 
 	new_node = (list_t *)malloc(sizeof(list_t));
 
 	if (new_node == NULL)
-		return NULL;
+		return (NULL);
 
 	new_node->str = strdup(str);
 
-	if (new_node->str == NULL) 
+	if (new_node->str == NULL)
 	{
 		free(new_node);
-		return NULL;
+		return (NULL);
 	}
 
 	new_node->len = _strlen(str);
