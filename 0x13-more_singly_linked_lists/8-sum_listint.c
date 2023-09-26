2947968606d7611a0dfeb36a46c78c0a9d002830
Ernesto Boca <boca.ernesto13@gmail.com>
2023-09-26 15:34:26 -0700

    8-sum_listint.c

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * sum_listint - returns the sum of all the data (n) of a list
+ * @head: the head of the list
+ * Return: the sum of all the data (n) of a list
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+	listint_t *temp = head;
+
+	if (head == NULL)
+		return (0);
+
+	while (temp != NULL)
+	{
+		sum += temp->n;
+		temp = temp->next;
+	}
+
+	return (sum);
+}
