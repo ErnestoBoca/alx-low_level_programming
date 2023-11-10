b9e46992b4cd1002435de7ea98c6be8132aecf6f
Ernesto Boca <boca.ernesto13@gmail.com>
2023-11-09 16:31:52 -0800

    TASSSSK 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -35,11 +35,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (idx >= get_size(*h))
 		return (NULL);
-	else if (idx == 0)
+	if (idx == 0)
 		return (add_dnodeint(h, n));
-	else if (idx == get_size(*h) - 1)
+	if (idx == get_size(*h) - 1)
 		return (add_dnodeint_end(h, n));
-	else
+	if (idx < get_size(*h))
 	{
 		temp = *h;
 		while (i < idx - 1)
@@ -59,4 +59,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 		return (new);
 	}
+
+	return (NULL);
 }
