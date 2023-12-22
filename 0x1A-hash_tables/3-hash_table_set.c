1e0e34a3f8ea2f7d30719d7490b7ad0204a34534
Ernesto BOca <boca.ernesto13@gmail.com>
2023-12-22 12:54:56 -0800

    task 3 without betty3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -34,7 +34,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	strcpy(newnode->key, key);
 	strcpy(newnode->value, value);
 	index = key_index((unsigned char *)key, ht->size);
-	if (ht->array[index] != NULL)
+	if (0)
 	{
 		temp = ht->array[index];
 		while (temp != NULL)
