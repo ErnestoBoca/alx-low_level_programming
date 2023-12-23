e054ff75a75a75a3f2ee0c6cc015d7b1bfec6758
Ernesto BOca <boca.ernesto13@gmail.com>
2023-12-22 16:27:34 -0800

    task 6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: the hash table
+ * Return: nothing
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *temp;
+
+	if (ht == NULL)
+		return;
+	for (i = 0; i < ht->size; i++)
+	{
+		while (ht->array[i] != NULL)
+		{
+			temp = ht->array[i]->next;
+			free(ht->array[i]->key);
+			free(ht->array[i]->value);
+			free(ht->array[i]);
+			ht->array[i] = temp;
+		}
+	}
+
+	free(ht->array);
+	free(ht);
+}
